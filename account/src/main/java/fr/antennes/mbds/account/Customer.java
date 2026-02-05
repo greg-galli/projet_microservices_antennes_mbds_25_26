81c2468df6ab51319abca522f4f0bc9b9c7a517b
Gregory Galli <greg.galli@gmail.com>
2026-02-05 15:30:43 +0100

    Circuit Breaker in Account Service

--- account/src/main/java/fr/antennes/mbds/account/Customer.java
+++ account/src/main/java/fr/antennes/mbds/account/Customer.java
@@ -1,9 +1,10 @@
 package fr.antennes.mbds.account;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
-@Data @AllArgsConstructor
+@Data @AllArgsConstructor @Builder
 public class Customer { // Une sorte de DTO
     private Long id;
     private String firstName;
--- account/src/main/java/fr/antennes/mbds/account/CustomerClient.java
+++ account/src/main/java/fr/antennes/mbds/account/CustomerClient.java
@@ -1,7 +1,7 @@
 package fr.antennes.mbds.account;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -11,8 +11,23 @@ import java.util.List;
 public interface CustomerClient {
 
     @GetMapping("/customers/{id}")
+    @CircuitBreaker(name = "customerById", fallbackMethod = "getDefaultCustomerById")
     Customer getCustomerById(@PathVariable Long id);
 
     @GetMapping("/customers")
+    @CircuitBreaker(name = "customers", fallbackMethod = "getDefaultCustomers")
     List<Customer> getCustomers();
+
+    default Customer getDefaultCustomerById(Long id, Exception exception) {
+        return Customer.builder()
+                .id(id)
+                .email("Not available")
+                .firstName("Not available")
+                .lastName("Not available")
+                .build();
+    }
+
+    default List<Customer> getDefaultCustomers(Exception exception) {
+        return List.of();
+    }
 }
